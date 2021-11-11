a23099d46300b8f53f33d41875a48975e3bdc6f9
JCubero-PA <tecnologiasdelainformacion@paloalto.com.ec>
2021-11-11 08:52:16 -0500

    movil usando video 'JC' 11/11/2021

--- App.js
+++ App.js
@@ -2,34 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import "./App.css";
-
+import { Video, AVPlaybackStatus } from 'expo-av';
 import logo from "./recursos/3Recurso 5.png";
 
 
 
 export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     < >
-    <div className="header1" >
-    <img className="logo" src={logo} />
-    <hr class="divider"></hr>
-    </div>
-    
-    <div className="body1" >
+      <div className="header1" >
+        <img className="logo" src={logo} />
+        <hr className="divider"></hr>
+      </div>
+
+      <div className="body1" >
 
         <p className="contenedortitle"> <span className="tittle0">floors for</span> <span className="tittle0">moments</span>  <span className="tittle0">of joy.</span> </p>
 
 
-       {/* <a className="title1">vive la</a>
+        {/* <a className="title1">vive la</a>
        <a className="title2"> experiencia </a>
        <a className="title3">  BERRYALLOC <br/></a> SDCFSDVC*/}
-       
-    </div>
-    <div className="iframevideo" >
-    <iframe  src="https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview" width="97%" height="450px" allow="autoplay"></iframe>
-    {/* <ReactPlayer url='https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview' /> */}
-    </div>
-</>
+
+      </div>
+      <div className="iframevideo" >
+        <iframe src="https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview" width="97%" height="450px" allow="autoplay"></iframe>
+        {/* <ReactPlayer url='https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview' /> */}
+      </div>
+      <div className="iframevideo1" >
+      <Video
+        ref={video}
+        // style={styles.video}
+        source={{
+          uri: 'https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <video src="https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview" autoplay poster="posterimage.jpg">
+  Tu navegador no admite el elemento <code>video</code>.
+</video>
+      </div>
+    </>
   );
 }
 //nsdflsndlf
@@ -40,7 +58,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header:{
+  header: {
     flex: 1,
     backgroundColor: '#fff',
 
