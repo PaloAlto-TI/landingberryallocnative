761454afa40a7385a0f77c9c4d131944c48d121c
JCubero-PA <tecnologiasdelainformacion@paloalto.com.ec>
2021-11-24 10:48:08 -0500

    mobil return prueba 'JC' v1 24/11/2021

---
+++ componentes/info/info.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Info = () => (
+  <>
+    <p className="info1" style={{color:"white"}}>ESPERA NUESTRA GALERIA SE CARGARA DESPUES DEL EVENTO ;)</p>
+  </>
+)
+export default Info
---
+++ componentes/viewer/viewer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ImageViewer from 'react-native-image-zoom-viewer';
+import { fotosid } from "../../recursos/fotosid"
+import { useLocation,useHistory } from 'react-router-dom';
+const Viewer = () => {
+    let location = useLocation();
+    let history = useHistory();
+// console.log("location: ",location);
+    let indexImg=0;
+   try{
+       
+       indexImg=location.state.detail
+   }
+   catch(e)
+   {
+     
+   }
+    
+    const images = [];
+    Object.keys(fotosid).forEach(e => {
+        images.push(
+            {
+                url: "https://drive.google.com/uc?id=" + fotosid[e].fotoid,
+                width: "90vw",
+                height: "40vh"
+            }
+
+        )
+
+        // console.log("que hay en e: ", [e])
+        // defaultValues[e] = "https://drive.google.com/uc?id=" + fotosid[e].fotoid;
+    });
+    return(
+  <>
+  <div style={{display:"flex",border:"1px solid white"}} >
+  <div style={{display:"inline-block",color:"white",float:"right",border:"1px solid white"}} onClick={()=>history.push("/fotos")}>
+      
+      X
+      </div>
+      </div>
+  <ImageViewer imageUrls={images} index={Number(indexImg)}  enableImageZoom={true} />
+
+  </>
+)}
+export default Viewer
