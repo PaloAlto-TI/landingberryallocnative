75e7f1a287f01f7f86cebae2defb9a21fcb11779
JCubero-PA <tecnologiasdelainformacion@paloalto.com.ec>
2021-11-26 16:06:27 -0500

    pruebas fotos mobile'JC' v2 26/11/2021

--- componentes/viewer/viewer.jsx
+++ componentes/viewer/viewer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ImageViewer from 'react-native-image-zoom-viewer';
-import { fotosid } from "../../recursos/fotosid"
+import { fotosid } from "../../recursos/fotosid";
 import { useLocation,useHistory } from 'react-router-dom';
+import imgBack from "../../recursos/goBack.png";
 const Viewer = () => {
     let location = useLocation();
     let history = useHistory();
-// console.log("location: ",location);
+
     let indexImg=0;
    try{
        
@@ -20,21 +21,20 @@ const Viewer = () => {
     Object.keys(fotosid).forEach(e => {
         images.push(
             {
+
                 url: "https://drive.google.com/uc?id=" + fotosid[e].fotoid,
                
             }
 
         )
 
-        // console.log("que hay en e: ", [e])
-        // defaultValues[e] = "https://drive.google.com/uc?id=" + fotosid[e].fotoid;
     });
     return(
   <>
-  <div style={{display:"flex",border:"1px solid white"}} >
-  <div style={{display:"inline-block",color:"white",float:"right",border:"1px solid white"}} onClick={()=>history.push("/fotos")}>
+  <div style={{display:"flex"}} >
+  <div style={{display:"inline-block",color:"white",float:"right"}} onClick={()=>history.goBack()}>
       
-      X
+  <img  src={imgBack} style={{height:"8vw",marginLeft:"1.8vw" ,border:"0.1vw solid grey",borderBottomLeftRadius:"2vw",borderBottomRightRadius:"2vw",borderTopLeftRadius:"2vw",borderTopRightRadius:"2vw"} } />
       </div>
       </div>
   <ImageViewer imageUrls={images} index={Number(indexImg)}  enableImageZoom={true} />
