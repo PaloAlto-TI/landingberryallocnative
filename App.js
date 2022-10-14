04bf6d578c3eccc03de74d70402bca52218e523c
PaloAlto-TI <tecnologiasdelainformacion@paloalto.com.ec>
2022-10-14 11:19:25 -0500

    landing prueba v2  - JC

--- App.js
+++ App.js
@@ -66,23 +66,7 @@ export default function App() {
       
     
 
-<<<<<<< HEAD
-        <p className="contenedortitle"> <span className="tittle0">floors for</span> <span className="tittle0">moments</span>  <span className="tittle0">of joy.</span> </p>
-
-
-       {/* <a className="title1">vive la</a>
-       <a className="title2"> experiencia </a>
-       <a className="title3">  BERRYALLOC <br/></a> SDCFSDVC*/}
-       
-    </div>
-    <div className="iframevideo" >
-    <iframe  src="https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview" width="97%" height="450px" allow="autoplay"></iframe>
-    {/* <ReactPlayer url='https://drive.google.com/file/d/1F2jGc8lW0DT-dF3UgQ__KJNdXiad5Qjy/preview'sdvsdvsd /> */}
-    </div>
-</>
-=======
     </>
->>>>>>> 12986b4c30279ff600a987876f9937c9067714dc
   );
 }
 
